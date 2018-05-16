4e431d45824fc940fa0b5d125a3a6f0645eff48f
campbelljos <joe.andrew.campbell@gmail.com>
2018-05-16 11:47:02 -0500

    Reconfigured files to be deployed into /var/www

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -1,4 +1,4 @@
-# $1 = OAUTH_TOKEN
+#!/usr/bin/env bash
 
 #Register the Microsoft key, register the product repository, and install required dependencies. 
 wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
@@ -7,20 +7,16 @@ dpkg -i packages-microsoft-prod.deb
 #Update the products available for installation, then install the .NET SDK
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1.200
-
-#Clone the site from the repository using passed OAUTH_TOKEN
-git clone https://$1:x-oauth-basic@github.com/campbelljos/personal_resume_website.git joe-resume-site
-cd "joe-resume-site/Resume Site"
+sudo apt-get install -y dotnet-sdk-2.1.200
 
 #Setup appache
-apt-get install apache2
+apt-get install -y apache2
 a2enmod proxy
 ufw allow in "Apache Full"
 
 service apache2 restart
+cd "/var/www/resume-site/Resume Site"
 a2ensite resume-site.conf
-
 
 dotnet build
 dotnet run
